7028716a8f0b4951897ea8cb7b7066589abe3792
fashaazhiputra <140335324+PersonalProejct@users.noreply.github.com>
2023-11-04 22:47:00 +0700

    Update Blog.js

--- src/data/Blog.js
+++ src/data/Blog.js
@@ -15,21 +15,6 @@ export const BlogData = [
       year: 2023
     }
   },
-  {
-    id: 2,
-    route: "Sejarah-stasiun-cicalengka",
-    title: "Sejarah stasiun cicalengka ",
-    subtile: "lorem iipsum dolor sit amet anjay slebew geming jajay totot",
-    article: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum animi tempora quas iste exercitationem nostrum alias voluptatibus laboriosam porro earum maiores magni, veniam numquam id sit repellendus doloribus natus a.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum animi tempora quas iste exercitationem nostrum alias voluptatibus laboriosam porro earum maiores magni, veniam numquam id sit repellendus doloribus natus a.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum animi tempora quas iste exercitationem nostrum alias voluptatibus laboriosam porro earum maiores magni, veniam numquam id sit repellendus doloribus natus a.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum animi tempora quas iste exercitationem nostrum alias voluptatibus laboriosam porro earum maiores magni, veniam numquam id sit repellendus doloribus natus a.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum animi tempora quas iste exercitationem nostrum alias voluptatibus laboriosam porro earum maiores magni, veniam numquam id sit repellendus doloribus natus a.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum animi tempora quas iste exercitationem nostrum alias voluptatibus laboriosam porro earum maiores magni, veniam numquam id sit repellendus doloribus natus a.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum animi tempora quas iste exercitationem nostrum alias voluptatibus laboriosam porro earum maiores magni, veniam numquam id sit repellendus doloribus natus a.",
-    gallery: {
-      img1: "https://cdn.discordapp.com/attachments/1169257091526045717/1170386390203240508/Medium_sized_JPEG.jpg?ex=6558da44&is=65466544&hm=6b0a113a19b973fab1b4e76f3f1ac452f3a4cd6dd14e7c766b910aacd3c627d0&",
-      img2: "/public/blog/reva_fidela.jpg"
-    },
-    author: {
-      date: 11,
-      month: "November",
-      year: 2023
-    }
-  },
+
 ]
 
